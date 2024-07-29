84952e2bb5df669adf2fa0b966a17a16857aced0
vinifrm <vinifrm@gmail.com>
2024-07-29 20:35:41 -0300

    refatoracao app.py e resources.py

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ from purchase_orders.resources import PurchaseOrders
 app = Flask(__name__) # armazena um nome único para esse arquivo; variável setada automaticamente pelo Python
 api = Api(app)
 
-
+# utilizar o flask restful para expor/configurar o endpoint que será relacionado ao recurso
+# usar a função add resource
 api.add_resource(PurchaseOrders, '/purchase_orders')
+# api.add_resource(PurchaseOrders, '/purchase_orders')
 
 app.run(port = 5000)
--- purchase_orders/resources.py
+++ purchase_orders/resources.py
@@ -2,19 +2,19 @@
 # também é uma boa prática inicializar a pasta como um módulo, com um arquivo __init__.py
 
 from flask import jsonify
-from flask_restful import Resource
+from flask_restful import Resource, reqparse # definir e resgatar conteudo do corpo de uma requisição
 
 purchase_orders = [
     { # dicionário que vai conter os pedidos de compra, com os itens pedidos
-    'id': 1,
-    'descricao': 'Pedido de compra',
-    'items': [
-        {
-            'id': 1,
-            'descricao': 'Item do pedido 1',
-            'preco': 20.99
-        }
-            ]
+        'id': 1,
+        'descricao': 'Pedido de compra',
+        'items': [
+            {
+                'id': 1,
+                'descricao': 'Item do pedido 1',
+                'preco': 20.99
+            }
+        ]
     } 
 ]
 
@@ -23,11 +23,33 @@ purchase_orders = [
 
 class PurchaseOrders(Resource):
 
+    parser = reqparse.RequestParser()
+    parser.add_argument( # define-se os parâmetros da requisição desejados
+        'id',
+        type = int,
+        required = True,
+        help = 'Informe um id' # mensagem caso o parâmetro esteja incorreto
+    ) 
+    parser.add_argument( # define-se os parâmetros da requisição desejados
+        'description',  # alterado para 'descricao' para manter consistência
+        type = str,
+        required = True,
+        help = 'Informe uma descrição' # mensagem caso o parâmetro esteja incorreto
+    ) 
+
     # já temos as funções get e post para serem sobrescritas
     # deve retornar um json em memória
     def get(self):
-
         return jsonify(purchase_orders)
 
-# utilizar o flask restful para expor/configurar o endpoint que será relacionado ao recurso
-# usar a função add resource
+    def post(self):
+        purchase_data = PurchaseOrders.parser.parse_args() # pegando os parâmetros inseridos no corpo da requisição
+        purchase_order = {
+            'id': purchase_data['id'],
+            'description': purchase_data['description'],  # alterado para 'descricao' para manter consistência
+            'items': []
+        }
+
+        purchase_orders.append(purchase_order)
+
+        return jsonify(purchase_order)
